e0c27633b6919055069a8a7ab1aef13654b1ffff
Nathan Park <nate.park90@gmail.com>
2016-01-21 19:27:31 -0800

    latest commit

--- app/controllers/qas_controller.rb
+++ app/controllers/qas_controller.rb
@@ -1,16 +1,19 @@
 class QasController < ApplicationController
 
-  def index
-    @qa = Qa.all
-  end
-
   def create
+
+    p params
+    
     athlete = Athlete.find_by_name(name: params[:name]).first_or_create(name: params[:host_name])
     host_id = athlete.host_id
 
     qa = Qa.new(host_id: host_id, start_time: params[:start_time], end_time: params[:end_time])
 
     qa.save
+  end
+
+  def show
+
   end
 
 end
